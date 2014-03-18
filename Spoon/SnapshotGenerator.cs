da2caf52b73871ca4f023d92bd5aa0157fb414ca
LeviBotelho <levi_botelho@hotmail.com>
2014-03-18 23:36:41 +0100

    Initial commit

---
+++ Spoon/SnapshotGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spoon
+{
+    public class SnapshotGenerator
+    {
+        readonly IEnumerable<string> _paths;
+
+        public SnapshotGenerator(IEnumerable<string> paths, string targetFolder)
+        {
+            _paths = paths;
+        }
+
+
+    }
+}
