64f2262820d314d4380cc3ec266089ae8e5a7379
Levi Botelho <levi_botelho@hotmail.com>
2014-04-09 14:42:26 +0200

    Added config file

---
+++ Spoon.Azure.SnapshotGenerator/src/config.js
@@ -0,0 +1,5 @@
+﻿exports.azureStorageAccount = '';
+exports.azureAccessKey = '';
+exports.azureContainerName = '';
+// Urls must be in hashbang form. (i.e. http://www.example.com/#!/about)
+exports.urls = [''];
