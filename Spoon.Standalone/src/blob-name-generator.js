66a8c4f7804c52cd7a5b97b42408bb9fe6834536
LeviBotelho <levi_botelho@hotmail.com>
2014-05-08 23:26:41 +0200

    Removed Spoon.Standalone (now in own repo)

--- Spoon.Standalone/src/blob-name-generator.js
+++
@@ -1,7 +0,0 @@
-﻿var namePrefix = '_escaped_fragment_=';
-exports.generate = function(url) {
-    var hashbangIndex = url.indexOf("#!");
-    var name = url.substr(hashbangIndex + 2);
-    if (name == '/') name = '';
-    return hashbangIndex == -1 ? namePrefix + '.html' : namePrefix + name + '.html';
-};
--- Spoon.Standalone/src/config.js
+++
@@ -1,9 +0,0 @@
-﻿exports.azureStorageAccount = 'angulartutorial';
-exports.azureAccessKey = 'yBQBNz5S5LxQkraLaTjjTJjxCcuJmVfmhl0YKMNeUtyRdxae5d/DVDFa1CkvAnSOY3umqyjp821cWiil26/8Ag==';
-exports.azureContainerName = 'spoon-snapshots';
-// The sitemap URL takes precedence over the url array. The sitemap must be a text file with one
-// URL per line. XML-style Sitemaps are not supported at this time.
-exports.sitemapUrl = 'http://www.learn-angular.org/Sitemap/Sitemap.txt';
-// All URLs must be in hashbang form (http://www.example.com/#!/about) with the exception of the
-// home page which does not have to, but can be.
-exports.urls = [];
